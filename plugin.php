23b2984c627d6c2ee6e075e87961c435703aa776
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:55:41 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_pregnancy_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Pregnancy Calculator by Calculator.iO";
 
 function display_ci_pregnancy_calculator(){
